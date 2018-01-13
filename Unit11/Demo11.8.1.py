86f09216de9c8ce5e62ba917712b062cd476a5ac
xiaogui-2017 <923445457@qq.com>
2018-01-14 00:08:13 +0800

    局部变量/全局变量

---
+++ Unit11/Demo11.8.1.py
@@ -0,0 +1,10 @@
+# -*- coding=utf-8 -*-
+all_par = 100
+
+
+def foo():
+    '如果赋予局部变量之前，你在函数中（为了防止这个全局变量）使用了这样名字，你将会得到一个异常，NAMEERROR/Unbound-LocalError'
+    print all_par
+    all_par = 10
+
+foo()
